88e333bd469a02082dfb3ab381b813789f026087
vinicius-ssantos <viniciusolicveira@gmail.com>
2022-04-10 00:14:48 -0300

    commit 10-04-2022

---
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long> {
+
+	List<Cozinha> findTodasByNomeContaining(String nome);
+	
+	Optional<Cozinha> findByNome(String nome);
+	
+	boolean existsByNome(String nome);
+	
+}
